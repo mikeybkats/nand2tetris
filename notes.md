b7e65608808e42133c7d923831e07882e235b7e4
mikeybkats <michaelbkats@gmail.com>
2021-11-18 21:10:57 -0800

    Adding notes

---
+++ notes.md
@@ -0,0 +1,19 @@
+# Unit 1.1 – boolean logic
+
+### Boolean identities
+
+_commutative laws_
+(x AND y) = (y AND x)
+(x OR y) = (y OR x)
+
+_associative laws_
+(x AND (y AND z)) = ((x AND y) AND z)
+(x OR (y OR z)) = ((x OR y) OR z)
+
+_distributive laws_
+(x AND (y OR z)) = (x AND y) OR (x AND z)
+(x OR (y AND z)) = (x OR y) AND (x OR z)
+
+_de Morgan laws_
+NOT(x AND y) = NOT(x) AND NOT(y)
+NOT(x OR y) = NOT(x) OR NOT(y)
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,41 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// int a
+// int b
+// int c = 0
+// 
+// for(i=0; i<b; i++){
+//   c = a+c
+// }
+// 
+// i    a   b   c
+// 0    4   5   4
+// 1            8
+// 2            12
+// 3            16
+// 4            20
+
+@i
+M=0
+@R2
+M=0
+
+(LOOP)
+  @R0
+  D=M
+  @R2
+  M=M+D
+  @i
+  M=M+1
+@R1
+D=M
+@i
+D=D-M
+@LOOP
+D;JGT
+
+(END)
+@END
+0;JEQ
