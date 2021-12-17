bd9840d0b58c4d9c73d88278d7d75a7287137710
mikeybkats <michaelbkats@gmail.com>
2021-12-17 08:58:24 -0800

    adding table builder

--- projects/06/assembler/symbol_table.py
+++ projects/06/assembler/symbol_table.py
@@ -3,19 +3,18 @@
 def build_default_table():
     global symbols
     symbols = dict([
-        ("R0", 0),
-        ("R1", 1),
-        ("R2", 2),
-        ("R3", 3),
-        ("R4", 4),
-        ("R5", 5),
-        ("R6", 6),
-        ("R7", 7),
-        ("R8", 8),
-        ("R9", 9),
-        ("R10", 10),
-        ("R11", 11),
+        ('SP', 0),
+        ('LCL', 1),
+        ('ARG', 2),
+        ('THIS', 3),
+        ('THAT', 4),
+        ('SCREEN', 16384),
+        ('KBD', 24576)
     ])
+    for n in range(16):
+        symbols['R'+str(n)] = n
+
+    return symbols
 
 
 def add_symbol_to_table(symbol, address):
