a91efd1717a4bdb1c11d61a9133034d8c39e03a5
mikeybkats <michaelbkats@gmail.com>
2022-05-18 09:28:33 -0700

    adding compilation engine

--- projects/JackAnalyzer/compilationEngine.py
+++ projects/JackAnalyzer/compilationEngine.py
@@ -1,14 +1,68 @@
+from JackTokenizer import JackTokenizer
+from TagTypes import Tag_Types
+from TokenTypes import Token_Type, Keyword
+import os
+
 
 class CompilationEngine(self):
     def __init__(self, inputFileStream, outputFileStream):
         """ 
         Creates a new compilation engin with the givin input and output. The next routine called must be compileClass()
         """
+
+        # make a jack tokenizer
+        self._tokenizer = JackTokenizer(inputStreamOrFile=inputFileStream)
+        # make the outfile
+        self._outfile = open(inputFileStream, mode="w+", encoding='utf-8')
+
         pass
+
+    def write_xml_tag(self, tagType):
+        self._outfile.write("<" + tagType + ">")
+
+    def write_xml_closing_tag(self, tagType):
+        self._outfile.write("</" + tagType + ">\n")
 
     def compile_class(self):
         """Compiles a complete class"""
-        pass
+        while self._tokenizer.has_more_tokens():
+            # get the token
+            self._tokenizer.advance()
+
+            if (self._tokenizer.token_type() == Token_Type.KEYWORD and
+                    self._tokenizer.currentToken == "class"):
+                self.write_xml_tag(Tag_Types.CLASS)
+                self._outfile.write("\n")
+                self.write_xml_tag(Tag_Types.KEYWORD)
+                self._outfile.write(self._tokenizer.currentToken)
+                self.write_xml_closing_tag(Tag_Types.KEYWORD)
+                break
+
+            if self._tokenizer.token_type() == Token_Type.IDENTIFIER:
+                self.write_xml_tag(Tag_Types.IDENTIFIER)
+                self._outfile.write(self._tokenizer.currentToken)
+                self.write_xml_closing_tag(Tag_Types.IDENTIFIER)
+                break
+
+            if (
+                self._tokenizer.token_type() == Token_Type.SYMBOL
+                    and (self._tokenizer.currentToken == "{"
+                         or self._tokenizer.currentToken == "}")
+            ):
+                self.write_xml_tag(Tag_Types.SYMBOL)
+                self._outfile.write(self._tokenizer.currentToken)
+                self.write_xml_closing_tag(Tag_Types.SYMBOL)
+                break
+
+            if (
+                self._tokenizer.token_type() == Token_Type.KEYWORD and
+                (
+                    self._tokenizer.currentToken == Keyword.STATIC or
+                    self._tokenizer.currentToken == Keyword.FIELD
+                )
+            ):
+                self.compile_class_var_declaration()
+                break
 
     def compile_class_var_declaration(self):
         """Compiles a static declaration or a field declaration"""
