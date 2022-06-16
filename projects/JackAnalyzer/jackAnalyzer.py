a173e09349e87cbdaee85ee9e94725faeda322ab
mikeybkats <michaelbkats@gmail.com>
2022-06-16 08:03:11 -0700

    compiler generally working

--- projects/JackAnalyzer/jackAnalyzer.py
+++ projects/JackAnalyzer/jackAnalyzer.py
@@ -10,19 +10,22 @@ class JackAnalyzer:
         self._input_loc = input_loc
 
         if os.path.isdir(input_loc):
+            self._path = input_loc
             self._fileList = os.listdir(input_loc)
 
     def parse_directory(self):
+        print("parsing directory:")
         # for each .jack file in the directory parse the file
         for fileName in self._fileList:
             if fileName.endswith(".jack"):
-                self._input_loc = fileName
+                self._input_loc = self._path + "/" + fileName
+                print("parsing file:", self._input_loc)
                 self.parse_file()
 
     def parse_file(self):
         # create a CompilationEngine from the xxx.jack input file
         outfile_path = self._input_loc.replace(".jack", ".xml")
-        print(outfile_path)
+        # print(outfile_path)
         self._comp_eng = CompilationEngine(input_stream=self._input_loc, output_stream=outfile_path)
         self._comp_eng.compile_class()
         self._comp_eng.close_outfile()
